7536b92dfd96645704aeb10d4ac76a14796ecde9
rutrum <dave@rutrum.net>
2025-03-06 19:22:35 -0500

    Movement and expiration as unix epoch

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "Artifacts MMO";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -9,10 +9,15 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-
     devShells.x86_64-linux.default = pkgs.mkShell {
       name = "artifactmmo";
-    };
 
+      buildInputs = with pkgs; [
+        just
+        jq
+      ];
+
+      API_URL="https://api.artifactsmmo.com";
+    };
   };
 }
